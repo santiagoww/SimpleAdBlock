6ce9eab7e3b1c1618171feeea43c5c2664f1a1f7
santiagoww <jmachon@gmail.com>
2015-09-27 17:58:40 +0200

    Simple ad blocker for iOS 9.
    Simple ad blocker for iOS 9. Feel free to copy, modify and contribute.
    Add filters to /ContentBlocker/blockerList.json

---
+++ README.md
@@ -0,0 +1,2 @@
+# SimpleAdBlock
+Ad blocker for iOS 9 as simple as it gets. Feel free to copy, modify and add filters to /ContentBlocker/blockerList.json
---
+++ SimpleAdBlock/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  SimpleAdBlock
+//
+//  Created by santiagoww on 27/09/15.
+//  Copyright © 2015 santiagoww. Feel free to copy and modify.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
